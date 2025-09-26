11e6348d3d8ee0056d3262b1547a73fe51af1423
Julian Hart <julian.w.hart@gmail.com>
2025-09-26 16:56:15 +1200

    Initial commit.  Basic scaffolding using vite react template.

---
+++ src/components/App/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import revoxLogo from '../../assets/revox-text-logo.png';
+
+function App() {
+
+  return (
+    <>
+      <div className='header'>
+        <img src={revoxLogo} />
+      </div>
+      <div className="post-container">
+
+      </div>
+    </>
+  )
+}
+
+export default App
